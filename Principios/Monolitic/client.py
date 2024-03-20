7d7bc84d5b65f358b1c013a0b453dd531c37e168
Iver011 <iver55pedro@gmail.com>
2024-03-20 13:49:26 -0400

    monolithic

---
+++ Principios/Monolitic/client.py
@@ -0,0 +1,27 @@
+import requests
+
+url='http://localhost:8000'
+
+response= requests.get(f"{url}/posts")
+print(response.text)
+
+
+response= requests.get(f"{url}/post/2")
+print(response.text)
+
+
+new_post_data = {
+    "title": "Mi experiencia como dev",
+    "content": "Es complicado",}
+
+
+response = requests.post(f"{url}/posts", json=new_post_data)
+print(response.text)
+
+response= requests.delete(f"{url}/post/2")
+print(response.text)
+
+response= requests.get(f"{url}/posts")
+print(response.text)
+
+response=requests.update()
